739019197f990f9a1f3fdc7335271dd5cb064868
Mark Wardle <mark@potient.com>
2019-11-02 20:38:00 -0600

    Remove coverage from config until I can figure out how to make it work

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,4 @@
-const path = require('path')
-
 module.exports = function (config) {
-  const IS_COVERAGE = process.env.NODE_ENV === 'coverage'
-
   const reporters = ['mocha']
   const plugins = [
     'karma-mocha',
@@ -12,39 +8,23 @@ module.exports = function (config) {
     'karma-chrome-launcher',
     'karma-firefox-launcher'
   ]
-  if (IS_COVERAGE) {
-    reporters.push('coverage-istanbul')
-    reporters.push('karma-coverage-istanbul-reporter')
+  const files = ['test/runner.js']
+  const preprocessors = {
+    'test/runner.js': ['rollup']
   }
   config.set({
     frameworks: ['mocha', 'chai'],
     browsers: ['Firefox'],
-    files: ['test/runner.js'],
+    files,
     reporters,
     plugins,
-    preprocessors: {
-      'test/runner.js': ['rollup']
-    },
+    preprocessors,
 
     rollupPreprocessor: {
       output: {
         format: 'iife', // Helps prevent naming collisions.
         name: 'factortest', // Required for 'iife' format.
         sourcemap: 'inline' // Sensible for testing.
-      }
-    },
-    coverageIstanbulReporter: {
-      reports: ['html', 'text-summary'],
-      dir: path.join(__dirname, 'coverage'),
-      combineBrowserReports: true,
-      skipFilesWithNoCoverage: false,
-      'report-config': {
-        html: {
-          subdir: 'html'
-        }
-      },
-      thresholds: {
-        emitWarning: false
       }
     }
   })
